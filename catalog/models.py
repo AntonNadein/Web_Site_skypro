eae4ce4f2df78eaaba037779ab13ede956c1edd4
Anton <fynjy-666@mail.ru>
2024-11-05 16:21:51 +0300

    install dotenv, psycopg2, pillow add setting DB add models migrate

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,34 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Наименование')
+    description = models.TextField(null=True, blank=True, verbose_name='Описание')
+    image = models.ImageField(upload_to='images/', verbose_name='Изображение')
+    category = models.ForeignKey('Category',
+                                 on_delete=models.SET(value='Категория не указана'),
+                                 related_name='category',
+                                 verbose_name='Категория')
+    price = models.FloatField(verbose_name='Цена за единицу')
+    created_at = models.DateField(auto_now=True, verbose_name='Дата создания')
+    updated_at = models.DateField(verbose_name='Дата изменения')
+
+    def __str__(self):
+        return f'{self.name} {self.price}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ['name', 'price', 'created_at']
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Наименование')
+    description = models.TextField(null=True, blank=True, verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
