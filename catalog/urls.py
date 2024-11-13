e4d07b605ad93718dbbf08de8138938a86913581
Anton <fynjy-666@mail.ru>
2024-11-13 15:17:56 +0300

    add form, add_pdroduct upd README

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,8 +6,10 @@ from . import views
 
 app_name = CatalogConfig.name
 
-urlpatterns = [path("", views.product_list, name="product_list"),
-               path("product_info/<int:product_pk>", views.product_info, name="product_info"),
-               path("contacts/", views.contacts, name="contacts"),
-               path("category/<int:category_pk>/", views.category_list, name="category"),
-               ]
+urlpatterns = [
+    path("", views.product_list, name="product_list"),
+    path("product_info/<int:product_pk>", views.product_info, name="product_info"),
+    path("contacts/", views.contacts, name="contacts"),
+    path("category/<int:category_pk>/", views.category_list, name="category"),
+    path("add_product/", views.add_product, name="add_product"),
+]
--- catalog/views.py
+++ catalog/views.py
@@ -1,23 +1,20 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-from catalog.models import Product, Category
+from catalog.forms import AddProductForm
+from catalog.models import Category, Product
 
 
 def product_list(request):
     products = Product.objects.all()
-    context = {
-        'products': products
-    }
-    return render(request, 'catalog/product_list.html', context=context)
+    context = {"products": products}
+    return render(request, "catalog/product_list.html", context=context)
 
 
 def product_info(request, product_pk):
     product = Product.objects.get(pk=product_pk)
-    context = {
-        'product': product
-    }
-    return render(request, 'catalog/product_info.html', context=context)
+    context = {"product": product}
+    return render(request, "catalog/product_info.html", context=context)
 
 
 def contacts(request):
@@ -33,8 +30,15 @@ def contacts(request):
 def category_list(request, category_pk):
     category = Category.objects.get(pk=category_pk)
     products = Product.objects.filter(category=category)
-    context = {
-        'category': category,
-        'products': products
-    }
+    context = {"category": category, "products": products}
     return render(request, "catalog/category_list.html", context=context)
+
+
+def add_product(request):
+    form = AddProductForm(request.POST)
+    context = {"form": form}
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+    return render(request, "catalog/add_product.html", context=context)
