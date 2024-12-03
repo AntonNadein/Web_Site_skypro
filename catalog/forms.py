4ba944a8dd1c0c9381da968af1276db8691f2794
Anton <fynjy-666@mail.ru>
2024-12-03 18:11:45 +0300

    upd lint

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,9 +1,19 @@
+import re
+
 from django import forms
+from django.core.exceptions import ValidationError
 
 from .models import Product
 
 
-class AddProductForm(forms.ModelForm):
+class MixinProduct:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs["class"] = "form-control"
+
+
+class AddProductForm(MixinProduct, forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -16,3 +26,51 @@ class AddProductForm(forms.ModelForm):
             "name": forms.TextInput(attrs={"class": "form-input"}),
             "description": forms.Textarea(attrs={"cols": 80, "rows": 10}),
         }
+
+    def clean_price(self):
+        price = self.cleaned_data.get("price")
+        if price < 0:
+            raise ValidationError("Цена не может быть отрицательной")
+        return price
+
+    def clean_image(self):
+        image = self.cleaned_data.get("image")
+        if image:
+            valid_formats = ["image/jpeg", "image/png"]
+            print(image.content_type)
+            if image.content_type not in valid_formats:
+                raise ValidationError("Неверный формат файла. Допустимые форматы: JPEG, PNG.")
+
+            size = image.size
+            if size > 1024 * 1024 * 70:
+                raise ValidationError("Размер файла превышает 5Мб")
+            return image
+
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get("name")
+        description = cleaned_data.get("description")
+        if self.validation_product(name):
+            raise ValidationError("В названии продукта присутствуют недопустимые слова")
+        elif self.validation_product(description):
+            raise ValidationError("В описании продукта присутствуют недопустимые слова")
+        else:
+            return cleaned_data
+
+    def validation_product(self, name):
+        list_exeption = [
+            "казино",
+            "криптовалюта",
+            "крипта",
+            "биржа",
+            "дешево",
+            "бесплатно",
+            "обман",
+            "полиция",
+            "радар",
+        ]
+        list_name = re.split(";|,| ", name)
+        for item in list_name:
+            if item.lower() in list_exeption:
+                return True
+        return False
